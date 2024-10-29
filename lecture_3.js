4c62d055f2bcdfc70324ed6bb03a398242f1db2f
ihortrosin <igor.trosin92@gmail.com>
2024-10-29 07:07:06 +0200

    slice 5 digit number lecture 3.3

--- lecture_3.js
+++ lecture_3.js
@@ -1,3 +1,4 @@
+// 3.1
 console.log("Тип даних Number:", typeof 32);
 console.log("Тип даних String:", typeof "string");
 console.log("Тип даних Boolean:", typeof true);
@@ -9,3 +10,12 @@ console.log("Тип даних Function:", typeof function() {});
 console.log("Тип даних Symbol:", typeof Symbol("symbol"));
 console.log("Тип даних BigInt:", typeof 123n);
 
+// 3.3 вирішив через слайс, хоча є способи красивіши просто нема часу
+let number = 25678;
+let numberStr = number.toString();
+let result = numberStr.slice(0, 1) + " " +
+    numberStr.slice(1, 2) + " " +
+    numberStr.slice(2, 3) + " " +
+    numberStr.slice(3, 4) + " " +
+    numberStr.slice(4, 5);
+console.log(result);
