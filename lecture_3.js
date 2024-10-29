ce73ff18c63b11fa1ed693a21833af8e73fc46cd
ihortrosin <igor.trosin92@gmail.com>
2024-10-29 07:03:48 +0200

    data types_ lecture 3.1

---
+++ lecture_3.js
@@ -0,0 +1,11 @@
+console.log("Тип даних Number:", typeof 32);
+console.log("Тип даних String:", typeof "string");
+console.log("Тип даних Boolean:", typeof true);
+console.log("Тип даних Undefined:", typeof undefined);
+console.log("Тип даних Null:", typeof null);
+console.log("Тип даних Object:", typeof { name: "Alice", age: 25 });
+console.log("Тип даних Array:", typeof [1, 2, 3]);
+console.log("Тип даних Function:", typeof function() {});
+console.log("Тип даних Symbol:", typeof Symbol("symbol"));
+console.log("Тип даних BigInt:", typeof 123n);
+
