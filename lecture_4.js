4e7dc8938f5d75cc9f36332a4df2ca876ae53f15
ihortrosin <igor.trosin92@gmail.com>
2024-10-29 10:15:23 +0200

    Homework lecture 4.2

--- lecture_4.js
+++ lecture_4.js
@@ -6,10 +6,33 @@ function calculateBirthYear() {
     alert("ваш рік народження" + birthYear)
 }
 calculateBirthYear();
-*/
+
  function helloCustomerName() {
      let customerName = prompt ("Введіть ваше імʼя");
     alert("Hello," + customerName + "! How are you?");
 
  }
 helloCustomerName();
+ */
+function numberVerification() {
+    const numberStr = prompt("Введіть тризначне число");
+    //const number = 222;
+    //let numberStr = number.toString();
+    let x = numberStr.slice(0, 1);
+    console.log(x);
+    let y = numberStr.slice(1, 2);
+    console.log(y);
+    let z = numberStr.slice(2, 3);
+    console.log(z);
+
+    if (x === y && y === z) {
+        alert("числа однакові");
+    }  else if (x === y || y === z || x === z) {
+        alert("є два однакових числа");
+    } else {
+        alert("числа не однакові");
+    }
+}
+numberVerification();
+
+
