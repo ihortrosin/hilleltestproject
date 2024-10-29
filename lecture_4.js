99bb9c42318974f795f780851c502913aac3ca43
ihortrosin <igor.trosin92@gmail.com>
2024-10-29 08:31:38 +0200

    Homework lecture 4.1

---
+++ lecture_4.js
@@ -0,0 +1,15 @@
+/*
+function calculateBirthYear() {
+    let currentYear = prompt("Який зараз рік?");
+    let age = prompt("Скільки вам років");
+    let birthYear = currentYear - age;
+    alert("ваш рік народження" + birthYear)
+}
+calculateBirthYear();
+*/
+ function helloCustomerName() {
+     let customerName = prompt ("Введіть ваше імʼя");
+    alert("Hello," + customerName + "! How are you?");
+
+ }
+helloCustomerName();
