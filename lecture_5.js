57d5ffa25d406fca16186243c29d617fc878bce0
ihortrosin <igor.trosin92@gmail.com>
2024-10-30 09:38:56 +0200

    Homework lecture 5.2

--- lecture_5.js
+++ lecture_5.js
@@ -1,5 +1,11 @@
+// // -- i+0.5 cycle
+// for (let i = 20; i < 30.5; i+=0.5) {
+//     console.log(i);
+// }
 
-for (let i = 20; i < 30.5; i+=0.5) {
-    console.log(i);
+// -- currency exchange rate cycle
+
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    let hryvnias = (dollars * 26);
+    console.log(`${dollars} ${hryvnias}`);
 }
-
