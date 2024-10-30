1372dcdaaae1d191cc7ab4ff408177c5daf3d72d
ihortrosin <igor.trosin92@gmail.com>
2024-10-30 08:16:27 +0200

    Homework lecture 4.3

--- lecture_4.js
+++ lecture_4.js
@@ -39,34 +39,34 @@ helloCustomerName();
 // -- 4.4
 
 
- let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-
-// if (numOrStr === null) {
-//     console.log('ви скасували')
-// } else if (numOrStr.trim() === '') {
-//     console.log('Empty String');
-// } else if (isNaN(+numOrStr)) {
-//     console.log(' number is Ba_NaN')
-// } else {
-// //     console.log('OK!')
+// // let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+//
+// // if (numOrStr === null) {
+// //     console.log('ви скасували')
+// // } else if (numOrStr.trim() === '') {
+// //     console.log('Empty String');
+// // } else if (isNaN(+numOrStr)) {
+// //     console.log(' number is Ba_NaN')
+// // } else {
+// // //     console.log('OK!')
+// // }
+//
+//
+// switch (true) {
+//     case (numOrStr === null):
+//         console.log('ви скасували');
+//         break;
+//     case (numOrStr.trim() === ''):
+//         console.log('Empty String');
+//         break;
+//     case (isNaN(+numOrStr)):
+//         console.log('number is Ba_NaN');
+//         break;
+//     default:
+//         console.log('OK!');
+//         break;
 // }
-
-
-switch (true) {
-    case (numOrStr === null):
-        console.log('ви скасували');
-        break;
-    case (numOrStr.trim() === ''):
-        console.log('Empty String');
-        break;
-    case (isNaN(+numOrStr)):
-        console.log('number is Ba_NaN');
-        break;
-    default:
-        console.log('OK!');
-        break;
-}
 
 //-- 4.3 Portrait
 
