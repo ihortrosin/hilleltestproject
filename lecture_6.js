d35f9cce1b688e148e521cb52f876a4fb7257ba3
ihortrosin <igor.trosin92@gmail.com>
2024-11-17 09:55:24 +0200

    Homework lecture 6.2

--- lecture_6.js
+++ lecture_6.js
@@ -1,9 +1,20 @@
 
-let str = "hello world";
-const symbolsToRemove = ["l", "d"];
-for (let i = 0; i <= 2; i++) {
-    const char = symbolsToRemove[i];
-    str = str.replaceAll(char, "");
-}
+// let str = "hello world";
+// const symbolsToRemove = ["l", "d"];
+// for (let i = 0; i <= 2; i++) {
+//     const char = symbolsToRemove[i];
+//     str = str.replaceAll(char, "");
+// }
+//
+// console.log(str);
 
-console.log(str);
+const mixedArray = [1, 'hi', false, 9, 126, {}, null, '10'];
+const numbers = mixedArray.filter(item => typeof item === 'number');
+let average;
+if (numbers.length > 0) {
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    average = sum / numbers.length;
+} else {
+    average = null;
+}
+console.log(average)
