1333b56e9d88b449dc5367de4df3337384275f59
ihortrosin <igor.trosin92@gmail.com>
2024-11-17 19:04:49 +0200

    Homework lecture 11.1

--- lecture_11.js
+++ lecture_11.js
@@ -19,3 +19,48 @@ function createPifagorTable() {
     document.body.appendChild(table);
 }
 createPifagorTable();
+
+// - 11.2
+
+const textElement = document.getElementById("text");
+const button = document.getElementById("toggleColorButton");
+let isColorChanged = false;
+button.addEventListener("click", function () {
+    if (isColorChanged) {
+        textElement.style.color = "black";
+    } else {
+
+        textElement.style.color = "blue";
+    }
+    isColorChanged = !isColorChanged;
+});
+
+
+// - 11.3
+document.addEventListener("DOMContentLoaded", function () {
+
+    const images = [
+        "1.jpeg",
+        "2.jpeg",
+        "3.jpeg",
+        "4.jpeg",
+        "5.jpeg",
+        "6.jpeg",
+        "7.jpeg",
+        "8.jpeg",
+        "9.jpeg"
+    ];
+
+    const randomImage = document.getElementById("randomKit");
+    const kitbutton = document.getElementById("randomImageButton");
+    function getRandomKit() {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const imagePath = `kit/${images[randomIndex]}`;
+        console.log("Шлях до зображення:", imagePath);
+        randomImage.src = imagePath;
+    }
+    getRandomKit();
+
+    kitbutton.addEventListener("click", getRandomKit);
+});
+
