a66ab97edf5567d8d85fdab2842dece17a6e0776
ihortrosin <igor.trosin92@gmail.com>
2024-11-17 10:21:40 +0200

    Homework lecture 7.1

--- lecture_6.js
+++ lecture_6.js
@@ -32,17 +32,5 @@
 // removeElement(array, 4);
 // console.log(array);
 
-// - 6.4
-function sum() {
-    let total = 0;
-    return function (num) {
-        total = total + num;
-        return total;
-    };
-}
-const add = sum();
-console.log(add(4));
-console.log(add(6));
-console.log(add(10));
-console.log(add(7));
+
 
--- lecture_7.js
+++ lecture_7.js
@@ -1,4 +1,18 @@
 // - 7.1
+// function sum() {
+//     let total = 0;
+//     return function (num) {
+//         total = total + num;
+//         return total;
+//     };
+// }
+// const add = sum();
+// console.log(add(4));
+// console.log(add(6));
+// console.log(add(10));
+// console.log(add(7));
+
+// - 7.2
 function name(a) {
     return function(b) {
         return a * b;
