9d4c487841ff5d7cfca431d9adfd9dbab745697d
ihortrosin <igor.trosin92@gmail.com>
2024-11-17 12:20:14 +0200

    Homework lecture 10.1

---
+++ lecture_10.js
@@ -0,0 +1,18 @@
+// - 10.1
+let user = {
+    name: "Ігор",
+    age: 32,
+    location: "Пафос",
+    email: "ihor@gmail.com",
+
+
+    showInfo: function() {
+        console.log(`Ім'я: ${this.name}`);
+        console.log(`Вік: ${this.age}`);
+        console.log(`Місце проживання: ${this.location}`);
+        console.log(`Email: ${this.email}`);
+    }
+};
+
+
+user.showInfo();
